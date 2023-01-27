37ba31e1f806de8001d6977ff5974f4216e09bd2
Gabriel Do Couto Santos <gabrieldocoutosantos@Gabriels-MacBook-Pro.local>
2023-01-27 08:57:02 -0300

    already fixing ios mobile behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 export default function Home() {
     return (
-    <div style={{ position: "absolute", width: "100vw", height: "100vh", top: 0, left:0}}>
         <object data=
                     "/dieta.pdf"
                 width="100%"
                 height="100%">
         </object>
-        </div>
     )
 }
 
