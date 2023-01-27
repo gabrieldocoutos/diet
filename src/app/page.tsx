1b2e1a87c5ed4476a7b294725faf5540e0cbcac0
Gabriel Do Couto Santos <gabrieldocoutosantos@Gabriels-MacBook-Pro.local>
2023-01-27 08:44:48 -0300

    lesgo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
     return (
+    <div style={{ position: "absolute", width: "100vw", height: "100vh", top: 0, left:0}}>
         <object data=
                     "/dieta.pdf"
-                width="800"
-                height="500">
+                width="100%"
+                height="100%">
         </object>
+        </div>
     )
 }
 
